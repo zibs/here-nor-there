39ca0567eef6b1a784d5169ca44838ede918b7a0
zibs <elizibin@gmail.com>
2017-09-22 22:45:55 -0700

    add media player. improve css

--- js/main.js
+++ js/main.js
@@ -1,58 +1,62 @@
 $(document).ready(function() {
-  $('#more-1').click(function() {
-    $('.more-1').toggleClass('hidden')
+  $("#more-1").click(function() {
+    $(".more-1").toggleClass("hidden");
     // if ($('.more-1').hasClass('hidden')) {
     //   $('.more-1').fadeIn().removeClass("hidden")
     // } else {
     //   $('.more-1').fadeOut().delay()addClass("hidden")
     // }
-    if ($('.more-1').hasClass('hidden')) {
-      $('#more-1').text('MORE')
+    if ($(".more-1").hasClass("hidden")) {
+      $("#more-1").text("MORE");
     } else {
-      $('#more-1').text('LESS')
+      $("#more-1").text("LESS");
     }
-  })
-  $('#more-2').click(function() {
-    $('.more-2').toggleClass('hidden')
-    if ($('.more-2').hasClass('hidden')) {
-      $('#more-2').text('MORE')
+  });
+  $("#more-2").click(function() {
+    $(".more-2").toggleClass("hidden");
+    if ($(".more-2").hasClass("hidden")) {
+      $("#more-2").text("MORE");
     } else {
-      $('#more-2').text('LESS')
+      $("#more-2").text("LESS");
     }
-  })
-  $('#more-3').click(function() {
-    $('.more-3').toggleClass('hidden')
-    if ($('.more-3').hasClass('hidden')) {
-      $('#more-3').text('MORE')
+  });
+  $("#more-3").click(function() {
+    $(".more-3").toggleClass("hidden");
+    if ($(".more-3").hasClass("hidden")) {
+      $("#more-3").text("MORE");
     } else {
-      $('#more-3').text('LESS')
+      $("#more-3").text("LESS");
     }
-  })
-})
+  });
+});
 
 $(document).ready(function() {
   // var player = document.getElementById('player')
   // document.getElementById('player').play()
-})
+});
 $(document).ready(function() {
-  var isPlaying = false
-  $('#ep1').click(function() {
-    var player = document.getElementById('epi1')
+  var isPlaying = false;
+  $("#ep1").click(function() {
+    var player = document.getElementById("epi1");
     if (isPlaying) {
-      document.getElementById('epi1').pause()
-      isPlaying = false
+      document.getElementById("epi1").pause();
+      isPlaying = false;
     } else {
-      document.getElementById('epi1').play()
-      isPlaying = true
+      document.getElementById("epi1").play();
+      isPlaying = true;
     }
-    player.addEventListener('timeupdate', function() {
-      var currentTime = player.currentTime
-      var duration = player.duration
-      $('.range')
+    player.addEventListener("timeupdate", function() {
+      var currentTime = player.currentTime;
+      var duration = player.duration;
+      $(".range")
         .stop(true, true)
-        .animate({ width: (currentTime + 0.25) / duration * 100 + '%' }, 250, 'linear')
-    })
-  })
+        .animate(
+          { width: (currentTime + 0.25) / duration * 100 + "%" },
+          250,
+          "linear"
+        );
+    });
+  });
   // var player = document.getElementById('player');
   // document.getElementById('player').play();
   // player.addEventListener("timeupdate", function() {
@@ -60,17 +64,34 @@ $(document).ready(function() {
   //     var duration = player.duration;
   //     $('.hp_range').stop(true,true).animate({'width':(currentTime +.25)/duration*100+'%'},250,'linear');
   // });
-})
+});
 
-var IS_PLAYING = false
+var IS_PLAYING = false;
 var CURRENT_ID;
 
 function pause() {
-  var oldPlayer = document.getElementById(CURRENT_ID)
-    oldPlayer.pause()
-    $(".slider").slideToggle();
-    $(".controls").fadeToggle();
-    IS_PLAYING = false
+  if (IS_PLAYING) {
+    var currentPlayer = document.getElementById(CURRENT_ID);
+    currentPlayer.pause();
+    IS_PLAYING = false;
+  }
+}
+
+function stop() {
+  var currentPlayer = document.getElementById(CURRENT_ID);
+  currentPlayer.pause();
+  currentPlayer.currentTime = 0;
+  $(".slider").slideToggle();
+  $(".controls").fadeToggle();
+  IS_PLAYING = false;
+}
+
+function play() {
+  if (!IS_PLAYING) {
+    var currentPlayer = document.getElementById(CURRENT_ID);
+    currentPlayer.play();
+    IS_PLAYING = true;
+  }
 }
 
 function playOrPause(id) {
@@ -79,33 +100,37 @@ function playOrPause(id) {
   }
 
   if (CURRENT_ID !== id && IS_PLAYING) {
-    pause()
+    pause();
   }
 
   CURRENT_ID = id;
 
-  var player = document.getElementById(id)
+  var player = document.getElementById(id);
   if (IS_PLAYING) {
-    player.pause()
+    player.pause();
     $(".slider").slideToggle();
     $(".controls").fadeToggle();
-    IS_PLAYING = false
+    IS_PLAYING = false;
   } else {
-    player.play()
+    player.play();
     $(".slider").slideToggle();
     $(".controls").fadeToggle();
-    attachListenerAndAnimateControls(id)
-    IS_PLAYING = true
+    attachListenerAndAnimateControls(id);
+    IS_PLAYING = true;
   }
 }
 
 function attachListenerAndAnimateControls(id) {
-  var player = document.getElementById(id)
-  player.addEventListener('timeupdate', function() {
-    var currentTime = player.currentTime
-    var duration = player.duration
-    $('.range')
+  var player = document.getElementById(id);
+  player.addEventListener("timeupdate", function() {
+    var currentTime = player.currentTime;
+    var duration = player.duration;
+    $(".range")
       .stop(true, true)
-      .animate({ width: (currentTime + 0.25) / duration * 100 + '%' }, 250, 'linear')
-  })
+      .animate(
+        { width: (currentTime + 0.25) / duration * 100 + "%" },
+        250,
+        "linear"
+      );
+  });
 }
